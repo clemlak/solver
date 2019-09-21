542a84477754aa2b17d7a654dff410faa2dddfad
Clement Lakhal <clementlakhal@gmail.com>
2019-09-21 11:58:43 +0200

    Add extension logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,109 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {
+  processStr,
+} from 'solhint/lib';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Solver activated...');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "solver" is now active!');
+  let timeout: NodeJS.Timer;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  const delay: number = vscode.workspace.getConfiguration().get('solver.delay') as number;
+  let solhintConfig: object;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+  const editor = vscode.window.activeTextEditor;
 
-	context.subscriptions.push(disposable);
+  const warningDecoration = vscode.window.createTextEditorDecorationType({
+    border: '1px solid #f39c12',
+    borderStyle: 'dashed'
+  });
+
+  const errorDecoration = vscode.window.createTextEditorDecorationType({
+    border: '1px solid #c0392b',
+    borderStyle: 'dashed'
+  });
+
+  if (vscode.workspace.name) {
+    vscode.workspace.findFiles('**/.solhint.json', '**/node_modules/**')
+      .then((files) => {
+        if (files.length > 0) {
+          const fs = vscode.workspace.fs;
+
+          fs.readFile(files[0])
+            .then((content) => {
+              console.log('Config file found!');
+              solhintConfig = JSON.parse(content.toString());
+              vscode.window.showInformationMessage('Loaded configuration from local solhint.json file.');
+            });
+        } else {
+          solhintConfig = vscode.workspace.getConfiguration().get('solver.config') as object;
+          vscode.window.showErrorMessage('No solhint.json config file has been found in the current workspace, the rules set in the settings will be used.');
+        }
+      });
+  } else {
+    vscode.window.showErrorMessage('The current file is not in a workspace, the rules set in the settings will be used.');
+    solhintConfig = vscode.workspace.getConfiguration().get('solver.config') as object;
+  }
+
+  function updateDecorations() {
+    if (!editor) {
+      return;
+    }
+
+    const warnings = [];
+    const errors = [];
+
+    const report = processStr(editor.document.getText(), solhintConfig);
+    console.log(report.messages);
+
+    for (let i = 0; i < report.messages.length; i += 1) {
+      const msg = report.messages[i];
+
+      const line = editor.document.lineAt(msg.line - 1);
+
+      const range = new vscode.Range(
+          new vscode.Position(msg.line - 1, msg.column - 1),
+          line.range.end
+      );
+
+      if (msg.severity === 3) {
+        const hoverMessage = `⚠️ Rule "${msg.ruleId}": ${msg.message}.`;
+
+        warnings.push({
+          hoverMessage,
+          range,
+        });
+      } else {
+        const hoverMessage = `❌ Rule "${msg.ruleId}": ${msg.message}.`;
+
+        errors.push({
+          hoverMessage,
+          range,
+        });
+      }
+    }
+
+    editor.setDecorations(warningDecoration, warnings);
+    editor.setDecorations(errorDecoration, errors);
+  }
+
+  function triggerUpdateDecorations() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(updateDecorations, delay);
+  }
+
+  vscode.window.onDidChangeActiveTextEditor((activeEditor) => {
+    if (activeEditor) {
+      triggerUpdateDecorations();
+    }
+  }, null, context.subscriptions);
+
+  vscode.window.onDidChangeTextEditorSelection(() => {
+    triggerUpdateDecorations();
+  }, null, context.subscriptions);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
